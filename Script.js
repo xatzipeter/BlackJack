5c50982705f76af264dc2bf8c63c79deb4b0bb30
xatzipeter <manos.chatz@hotmail.com>
2019-03-03 20:05:34 +0200

    Add project files.

---
+++ Script.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+
+let deck = [
+    "Ace of Spades",
+    "Two of Spades",
+    "Three of Spades"
+];
+
+let playerCards = [deck[0], deck[2]];
+
+console.log("You are dealt: ")
+console.log(" " + playerCards[0]);
+console.log(" " + playerCards[1]);
