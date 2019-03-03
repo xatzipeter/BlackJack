9e5b98a7d007577b65929fda94ac4bf0ad988c27
xatzipeter <manos.chatz@hotmail.com>
2019-03-03 20:35:43 +0200

    arrays added

--- BlackJack/Script.js
+++ BlackJack/Script.js
@@ -1,13 +1,23 @@
 // JavaScript source code
 
-let deck = [
-    "Ace of Spades",
-    "Two of Spades",
-    "Three of Spades"
-];
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+let values = ['Ace', 'King', 'Queen', 'Jack',
+    'Ten', 'Nine', 'Eight', 'Seven', 'Six',
+    'Five', 'Four', 'Three', 'Two', 'One'];
 
-let playerCards = [deck[0], deck[2]];
+let deck = [];
 
-console.log("You are dealt: ")
-console.log(" " + playerCards[0]);
-console.log(" " + playerCards[1]);
+for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+    for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+        deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+    }
+}
+
+function showMessage(message) {
+    console.log(message);
+}
+
+for (let i = 0; i < deck.length; i++) {
+    console.log(deck[i]);
+}
+showMessage();
