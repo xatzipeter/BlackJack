c82d29b2eb3d51988806df5e53b73d0e7704ef46
xatzipeter <manos.chatz@hotmail.com>
2019-03-04 09:00:13 +0200

    Copy from course

--- Script.js
+++ Script.js
@@ -1,13 +1,38 @@
 // JavaScript source code
 
-let deck = [
-    "Ace of Spades",
-    "Two of Spades",
-    "Three of Spades"
-];
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+let values = ['Ace', 'King', 'Queen', 'Jack',
+    'Ten', 'Nine', 'Eight', 'Seven', 'Six',
+    'Five', 'Four', 'Three', 'Two'];
 
-let playerCards = [deck[0], deck[2]];
 
-console.log("You are dealt: ")
-console.log(" " + playerCards[0]);
-console.log(" " + playerCards[1]);
+
+function createDeck() {
+    let deck = [];
+    for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+        for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+            deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+        }
+    }
+    return deck;
+}
+
+function getNextCard() {
+    return deck.shift();
+}
+
+let deck = createDeck();
+
+
+
+for (let i = 0; i < deck.length; i++) {
+    console.log(deck[i]);
+}
+
+let playerCards = [getNextCard(), getNextCard()];
+
+console.log("Welcome to Blackjack!");
+
+console.log("You are dealt: ");
+console.log("  " + playerCards[0]);
+console.log("  " + playerCards[1]);
