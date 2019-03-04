3d563bda55da9fb3be839261b0b3c6029f212794
xatzipeter <manos.chatz@hotmail.com>
2019-03-04 10:11:29 +0200

    Shuffling

--- Script.js
+++ Script.js
@@ -11,7 +11,11 @@ function createDeck() {
     let deck = [];
     for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
         for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
-            deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+            let card = {
+                suit: suits[suitIdx],
+                value: values[valueIdx]
+            };
+            deck.push(card);
         }
     }
     return deck;
@@ -21,18 +25,28 @@ function getNextCard() {
     return deck.shift();
 }
 
+function getCardString(card) {
+    return card.value + ' ' + card.suit;
+}
+
 let deck = createDeck();
 
 
 
-for (let i = 0; i < deck.length; i++) {
-    console.log(deck[i]);
-}
+//for (let i = 0; i < deck.length; i++) {
+//    console.log(deck[i]);
+//}
 
 let playerCards = [getNextCard(), getNextCard()];
+
+let textArea = document.getElementById('text-area');
+let okButton = document.getElementById('ok-button');
+okButton.addEventListener('click', function () {
+    textArea.innerText = 'Button clicked!';
+});
 
 console.log("Welcome to Blackjack!");
 
 console.log("You are dealt: ");
-console.log("  " + playerCards[0]);
-console.log("  " + playerCards[1]);
+console.log("  " + getCardString(playerCards[0]));
+console.log("  " + getCardString(playerCards[1]));
